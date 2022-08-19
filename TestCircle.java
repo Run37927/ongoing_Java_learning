9b16777fbe2e04488b7855317d6d53cd89106963
Hairun Huang <hairunhuang@gmail.com>
2022-08-18 20:35:27 -0700

    create circle object

---
+++ TestCircle.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(new Point(1,2), 3);
+
+        System.out.println( c1.getArea() );
+
+    }
+}
